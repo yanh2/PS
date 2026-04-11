12ca136f7798e0929e9a3b17dbe704ffa2750327
yanha <yanha14@naver.com>
2026-04-11 21:03:21 +0900

    [1012] 유기농 배추 / 실버2

--- BOJ/1012.py
+++ BOJ/1012.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
 
 dx = [0, 0, 1, -1]
@@ -13,22 +14,38 @@ def dfs(x, y, board, visit):
         if not visit[nx][ny] and board[nx][ny]==1:
             dfs(nx, ny, board, visit)
 
+def bfs(q, board, visit):
+    while q:
+        a, b = q.popleft()
+        for i in range(4):
+            nx = a+dx[i]
+            ny = b+dy[i]
+            if nx<0 or nx >=len(board) or ny<0 or ny >=len(board[0]): continue
+            if not visit[nx][ny] and board[nx][ny]==1:
+                visit[nx][ny] = True
+                q.append((nx, ny))
+
 t = int(input())
 while t:
     m, n, k = map(int,input().split())
-    cnt = 0
+    answer = 0
     board = [[0]*m for _ in range(n)]
     visit = [[False]*m for _ in range(n)]
+    q = deque()
+
     for _ in range(k):
         y, x =map(int, input().split())
         board[x][y]=1
-
+        
     for i in range(n):
         for j in range(m):
             if not visit[i][j] and board[i][j]==1:
-                cnt += 1
-                dfs(i, j, board, visit)
+                answer += 1
+                #dfs(i, j, board, visit)
+                q.append((i,j))
+                visit[i][j] = True
+                bfs(q, board, visit)
     
-    print(cnt)
+    print(answer)
 
     t-=1
