8f4d96b9d23ed7c2e688ec5e2d0d53708f608ec5
hayan <yanha14@naver.com>
2026-05-12 17:35:49 +0900

    완주하지 못한 선수 / Lv.1

---
+++ PGS/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0; i<participant.length; i++){
+            map.put(participant[i], map.getOrDefault(participant[i],0)+1);
+        }
+
+        for(int i=0; i<completion.length; i++){
+            map.put(completion[i], map.get(completion[i])-1);
+        }
+        for(String k : map.keySet()){
+            if(map.get(k)==1){
+                answer = k; 
+            }
+            
+        }
+
+        return answer;
+    }
+}
